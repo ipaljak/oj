2e8a4bdbd31f7855bdb59ba8192c7ee0f5fc16a3
Ivan Paljak <ipaljak@gmail.com>
2015-09-18 00:18:00 +0200

    Add solution for problem `A - Raising Bacteria` Link: https://codeforces.com/contest/579/problem/A Tags: bitmasks, *1000 Ref: https://codeforces.com/contest/579/submission/13076818

---
+++ codeforces/579/A.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+typedef long long llint;
+typedef pair<int, int> pii;
+
+int x;
+
+int main(void) {
+
+    scanf("%d", &x);
+    printf("%d\n", __builtin_popcount(x));
+
+    return 0;
+
+}
+
