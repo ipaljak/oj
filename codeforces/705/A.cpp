ff2e5e0ab56e35fcee6748dd744966cd2f20d55e
Ivan Paljak <ipaljak@gmail.com>
2016-08-07 16:16:00 +0200

    Add solution for problem `A - Hulk` Link: https://codeforces.com/contest/705/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/705/submission/19691871

---
+++ codeforces/705/A.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+#define TRACE(x) cerr << #x << " " << x << endl
+#define FOR(i, a, b) for (int i = (a); i < int(b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define _ << " " <<
+
+#define fst first
+#define snd second
+
+typedef long long llint;
+typedef pair<int, int> pii;
+
+const int B = 27397, MOD = 1e9 + 7;
+const int B1 = 33941, MOD1 = 1e9 + 9;
+
+int n;
+
+int main(void) {
+
+  scanf("%d", &n);
+  for (int i = 0; i < n; ++i) {
+    if (i > 0) 
+      printf(" that ");
+    if (i & 1) 
+      printf("I love");
+    else
+      printf("I hate");
+  }
+
+  printf(" it\n");
+
+  return 0;
+
+}
+
