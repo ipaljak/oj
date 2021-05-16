82f9720db977de987089f13b272a075be661afb6
Ivan Paljak <ipaljak@gmail.com>
2021-05-16 10:02:00 +0200

    Add solution for problem `A - Potion-making` Link: https://codeforces.com/contest/1525/problem/A Tags: math, number theory Ref: https://codeforces.com/contest/1525/submission/116320890

---
+++ codeforces/1525/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define TRACE(x) cerr << #x << " " << x << endl
+#define _ << " " <<
+
+template <class> struct is_container : false_type {};
+template <class... Ts> struct is_container<vector<Ts...>> : true_type {};
+template <class... Ts> struct is_container<map<Ts...>> : true_type {};
+template <class... Ts> struct is_container<unordered_map<Ts...>> : true_type {};
+template <class... Ts> struct is_container<list<Ts...>> : true_type {};
+template <class... Ts> struct is_container<set<Ts...>> : true_type {};
+template <class... Ts> struct is_container<unordered_set<Ts...>> : true_type {};
+template <class T, class = typename enable_if<is_container<T>::value>::type>
+ostream& operator << (ostream &o, T x) {
+  int f = 1;
+  o << "[";
+  for (auto y : x) {
+    o << (f ? "" : ", ") << y;
+    f = 0;
+  }
+  return o << "]";
+}
+template<class T, class U>
+ostream& operator<< (ostream &o, pair<T, U> x) {
+  return o << "(" << x.first << ", " << x.second << ")";
+}
+
+typedef long long llint;
+
+const int MOD = 1e9 + 7;
+
+int t;
+
+int main(void) {
+  scanf("%d", &t);
+  while (t--) {
+    int k;
+    scanf("%d", &k);
+    if (k == 0 || k == 100) {
+      printf("1\n");
+      continue;
+    }
+    int a = k, b = 100 - k;
+    int g = __gcd(a, b);
+    a /= g; b /= g;
+    printf("%d\n", a + b);
+  }
+  return 0;
+}
